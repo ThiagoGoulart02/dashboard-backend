290933b04e043718d714b9a47ac7cac5c476786a
Thiago Goulart <thiago.goulart001@edu.pucrs.br>
2024-03-07 21:31:04 -0300

    Creating the user validator with password and email validation

--- dashboard/src/main/java/com/example/dashboard/validator/UserValidator.java
+++ dashboard/src/main/java/com/example/dashboard/validator/UserValidator.java
@@ -4,25 +4,32 @@ import com.example.dashboard.exceptions.ValidationException;
 import com.example.dashboard.web.representation.request.user.RequestUser;
 import org.springframework.stereotype.Component;
 
+
 @Component
 public class UserValidator {
 
+    private final int MIN_PASSWORD_SIZE = 7;
+
     public void validatePayload(RequestUser payload){
         if(!validateEmail(payload.email())){
-            //exception
+            throw new ValidationException("The email is invalid.");
         }
 
-        if(!validatePassword(payload.password())){
+        if(!isPasswordValida(payload.password())){
             throw new ValidationException("The password is invalid.");
         }
 
     }
 
     public boolean validateEmail(String email){
-        return false;
+        return email.contains("@") && email.contains("gmail.com");
     }
-    public boolean validatePassword(String password){
-        return false;
+    public boolean isPasswordValida(String password){
+        return password.length()>=MIN_PASSWORD_SIZE &&
+                password.chars().anyMatch(Character::isLetter) &&
+                password.chars().anyMatch(Character::isDigit) &&
+                password.chars().anyMatch(Character::isUpperCase) &&
+                password.chars().anyMatch(Character::isLowerCase);
     }
 
 }
